60976898ab8cdadaf55547bb9deb1a16914f658a
Rob Patterson <rob.patterson@vistavusolutions.com>
2021-12-11 18:19:03 -0700

    Brought over some basic html stuff for the game.

--- src/HomePage.elm
+++ src/HomePage.elm
@@ -22,10 +22,81 @@ view model =
             ]
 
 
+gameview : Model -> Html Msg
 gameview model =
-    div []
-        [ text "Game has started"
+    let
+        ( leftCounter, centerCounter, rightCounter ) =
+            model.counterWith
+    in
+    div [ class "game-container" ]
+        [ div [ class "title-section" ]
+            [ h1 [ class "center" ] [ text "PokeType Battle" ]
+            , h2 [ class "center" ] [ text ("Score: " ++ String.fromInt model.score) ]
+            , p [ class "center" ] [ text ("Time Remaining: " ++ String.fromInt model.remainingseconds) ]
+            , div
+                [ style "display" "flex"
+                , style "justify-content" "center"
+                ]
+                [ if model.remainingseconds == 0 then
+                    button
+                        [ style "width" "300px"
+                        , style "height" "40px"
+                        , onClick Start
+                        ]
+                        [ text "Play Again?" ]
+
+                  else
+                    case model.lastRoundSummary of
+                        Just summary ->
+                            div
+                                [ class "center"
+                                , style "background-color" summary.color
+                                , style "min-width" "300px"
+                                , style "min-height" "40px"
+                                ]
+                                [ text summary.message ]
+
+                        Nothing ->
+                            div [] []
+                ]
+            ]
+        , if model.remainingseconds == 0 then
+            div [] []
+
+          else
+            div
+                [ class "opponent center"
+                , style "margin" "30px"
+                , style "background-color" (getColor model.opponent)
+                ]
+                [ div []
+                    [ p [] [ text "Opponent is:" ]
+                    , p [ class "type-name" ] [ text (getName model.opponent) ]
+                    ]
+                ]
+        , counterDiv "verses1 center" leftCounter model
+        , counterDiv "verses2 center" centerCounter model
+        , counterDiv "verses3 center" rightCounter model
         ]
+
+
+counterDiv : String -> PokeType -> Model -> Html Msg
+counterDiv classStr pokeType model =
+    if model.remainingseconds == 0 then
+        div [] []
+
+    else
+        button
+            [ class classStr
+            , style "background-color" (getColor pokeType)
+            --, onClick (CounterWith pokeType)
+            ]
+            [ div []
+                [ p [] [ text "Counter with:" ]
+                , p [ class "type-name" ] [ text (getName pokeType) ]
+                ]
+            ]
+
 
 
 type Msg
